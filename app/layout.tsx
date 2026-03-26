d21f920ec79bceb3dfdb4c19e8447b5959a3666d
damowang-cxx <dtghostzxl@163.com>
2026-03-26 17:52:59 +0800

    新增图标

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,21 @@ import { TranslationProvider } from "@/contexts/TranslationContext";
 import { DictationModeProvider } from "@/contexts/DictationModeContext";
 import { FocusModeProvider } from "@/contexts/FocusModeContext";
 
+const BASE_PATH = "/listen";
+
 export const metadata: Metadata = {
   title: "英语学习训练",
   description: "英语学习训练网站 - 通过音频和句子分段学习英语",
+  icons: {
+    icon: [
+      { url: `${BASE_PATH}/Learnico16x16.ico`, sizes: "16x16", type: "image/x-icon" },
+      { url: `${BASE_PATH}/Learnico32x32.ico`, sizes: "32x32", type: "image/x-icon" },
+      { url: `${BASE_PATH}/Learnico48x48.ico`, sizes: "48x48", type: "image/x-icon" },
+      { url: `${BASE_PATH}/Learnico.png`, type: "image/png" },
+    ],
+    shortcut: `${BASE_PATH}/Learnico32x32.ico`,
+    apple: `${BASE_PATH}/Learnico.png`,
+  },
 };
 
 export default function RootLayout({
@@ -27,9 +39,7 @@ export default function RootLayout({
               <ParticleBackground />
               <CockpitPanel />
               <div className="scan-line" />
-              <div className="relative z-10">
-                {children}
-              </div>
+              <div className="relative z-10">{children}</div>
             </DictationModeProvider>
           </FocusModeProvider>
         </TranslationProvider>
